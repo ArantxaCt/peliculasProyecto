b7baf45af95ecfef93de1518fa70e7417df03a66
ArantxaCt <arantxa.ect@gmail.com>
2022-09-19 11:33:46 -0500

    secondview elements

---
+++ PeliculasProyecto/Controller/DetallePeliculaViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetallePeliculaViewController.swift
+//  PeliculasProyecto
+//
+//  Created by Arantxa Emanth Cuellar Torres on 19/09/22.
+//
+
+import UIKit
+
+class DetallePeliculaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //MARK: Cambiamos el color de nuestro botón en el navigation controller
+        navigationController?.navigationBar.tintColor = UIColor.white
+    }
+
+}
